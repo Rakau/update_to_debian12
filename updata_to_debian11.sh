2be682310cc7b2167446ab5d06f1f44235bcf5e3
Rakau <luojiayu09@gmail.com>
2021-08-22 21:11:36 +0800

    Update updata_to_debian11.sh

--- updata_to_debian11.sh
+++ updata_to_debian11.sh
@@ -13,20 +13,30 @@ function menu ()
 {
  cat << EOF
 ----------------------------------------
-|******请选择机器网络环境所在地******|
+|*********请选择机器当前版本*********|
 ----------------------------------------
-`echo -e "\033[35m 1)其他地区\033[0m"`
-`echo -e "\033[35m 2)中国大陆\033[0m"`
+`echo -e "\033[35m 1)Debian 7\033[0m"`
+`echo -e "\033[35m 2)Debian 8\033[0m"`
+`echo -e "\033[35m 3)Debian 9\033[0m"`
+`echo -e "\033[35m 4)Debian 10\033[0m"`
 EOF
 read -p "请输入对应地区数字：" num1
 case $num1 in
  1)
-  echo "你选择了其它地区"
-  echo -e "----------更新当前系统版本软件包----------" && apt update && apt upgrade -y && echo -e "----------删除未使用的依赖项----------" && apt --purge autoremove && cp -i /etc/apt/sources.list /etc/apt/sources.list.original && rm /etc/apt/sources.list && touch /etc/apt/sources.list && echo -e "deb http://deb.debian.org/debian bullseye main contrib non-free\ndeb-src http://deb.debian.org/debian bullseye main contrib non-free\ndeb http://deb.debian.org/debian-security/ bullseye-security main contrib non-free\ndeb-src http://deb.debian.org/debian-security/ bullseye-security main contrib non-free\ndeb http://deb.debian.org/debian bullseye-updates main contrib non-free\ndeb-src http://deb.debian.org/debian bullseye-updates main contrib non-free">/etc/apt/sources.list && echo -e "----------更新到 Debian 11----------" && apt update && apt full-upgrade && echo -e "----------脚本执行完毕，请自行重启系统应用新版本（以防万一，建议备份重要文件）----------"
+  echo "你选择了Debian 7"
+  echo -e "----------更新当前系统版本软件包----------" && apt update && apt upgrade -y && echo -e "----------删除未使用的依赖项----------" && apt --purge autoremove && cp -i /etc/apt/sources.list /etc/apt/sources.list.original && sed -i 's/wheezy/bullseye/g' /etc/apt/sources.list && echo -e "----------更新到 Debian 11----------" && apt update && apt full-upgrade && echo -e "----------脚本执行完毕，请自行重启系统应用新版本（以防万一，建议备份重要文件）----------"
   ;;
  2)
-  echo "你选择了中国大陆"
-  echo -e "----------更新当前系统版本软件包----------" && apt update && apt upgrade -y && echo -e "----------删除未使用的依赖项----------" && apt --purge autoremove && cp -i /etc/apt/sources.list /etc/apt/sources.list.original && rm /etc/apt/sources.list && touch /etc/apt/sources.list && echo -e "deb http://mirrors.ustc.edu.cn/debian bullseye main contrib non-free\ndeb-src http://mirrors.ustc.edu.cn/debian bullseye main contrib non-free\ndeb http://mirrors.ustc.edu.cn/debian-security/ bullseye-security main contrib non-free\ndeb-src http://mirrors.ustc.edu.cn/debian-security/ bullseye-security main contrib non-free\ndeb http://mirrors.ustc.edu.cn/debian bullseye-updates main contrib non-free\ndeb-src http://mirrors.ustc.edu.cn/debian bullseye-updates main contrib non-free">/etc/apt/sources.list && echo -e "----------更新到 Debian 11----------" && apt update && apt full-upgrade && echo -e "----------脚本执行完毕，请自行重启系统应用新版本（以防万一，建议备份重要文件）----------"
+  echo "你选择了Debian 8"
+  echo -e "----------更新当前系统版本软件包----------" && apt update && apt upgrade -y && echo -e "----------删除未使用的依赖项----------" && apt --purge autoremove && cp -i /etc/apt/sources.list /etc/apt/sources.list.original && sed -i 's/jessie/bullseye/g' /etc/apt/sources.list && echo -e "----------更新到 Debian 11----------" && apt update && apt full-upgrade && echo -e "----------脚本执行完毕，请自行重启系统应用新版本（以防万一，建议备份重要文件）----------"
+  ;;
+ 3)
+  echo "你选择了Debian 9"
+  echo -e "----------更新当前系统版本软件包----------" && apt update && apt upgrade -y && echo -e "----------删除未使用的依赖项----------" && apt --purge autoremove && cp -i /etc/apt/sources.list /etc/apt/sources.list.original && sed -i 's/stretch/bullseye/g' /etc/apt/sources.list && echo -e "----------更新到 Debian 11----------" && apt update && apt full-upgrade && echo -e "----------脚本执行完毕，请自行重启系统应用新版本（以防万一，建议备份重要文件）----------"
+  ;;
+ 4)
+  echo "你选择了Debian 10"
+  echo -e "----------更新当前系统版本软件包----------" && apt update && apt upgrade -y && echo -e "----------删除未使用的依赖项----------" && apt --purge autoremove && cp -i /etc/apt/sources.list /etc/apt/sources.list.original && sed -i 's/buster/bullseye/g' /etc/apt/sources.list && echo -e "----------更新到 Debian 11----------" && apt update && apt full-upgrade && echo -e "----------脚本执行完毕，请自行重启系统应用新版本（以防万一，建议备份重要文件）----------"
   ;;
 esac
 }
